3ecc00334b6e4b0e26e29256764a1de8b94a60a4
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-19 14:56:27 -0600

    created image table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_203151) do
+ActiveRecord::Schema.define(version: 2018_05_19_205357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/models/idea_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-describe Idea, type: :model do
-  describe 'validations' do
-    it {should belong_to(:category)}
-  end
-end
