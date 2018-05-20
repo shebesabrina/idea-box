35069867599754dd90d5ce7a67dcfae29469afb0
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-19 18:26:28 -0600

    Added a create method for an idea.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,26 @@
 class IdeasController < ApplicationController
 
+  def index
+    @ideas = Idea.all
+  end
+
   def new
     @idea = Idea.new
+  end
+
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
+  def create
+    idea = Idea.create(idea_params)
+
+    redirect_to idea_path(idea)
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :description)
   end
 end
---
+++ spec/features/idea/create_a_new_idea_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+# An idea can be created by a user.
+describe 'user new page' do
+  it 'should allow user to create an idea' do
+
+    visit new_idea_path
+
+    expect(page).to have_button("Create Idea")
+
+    fill_in 'idea[title]', with: 'New Title'
+    fill_in 'idea[description]', with: 'New Description'
+    click_on 'Create Idea'
+
+    expect(current_path).to eq(ideas_path)
+    expect(page).to have_content('New Title')
+    expect(page).to have_content('New Description')
+  end
+end
