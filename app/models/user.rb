c41996dcdd931e8923659f1f6d7d7d19a680646b
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-21 16:15:58 -0600

    Ian told me to.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :name, presence: true
+
+  has_secure_password
   # e.g., User.authenticate('penelope@turing.com', 'boom')
   def self.authenticate(email, password)
     # if email and password correspond to a valid user, return that user
