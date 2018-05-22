3a57132c5891141e68a204d3f978ca72133a11f0
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-22 11:38:53 -0600

    Fixed log out tests with a stub in test

--- spec/features/user/user_can_sign_up_spec.rb
+++ spec/features/user/user_can_sign_up_spec.rb
@@ -4,23 +4,21 @@ describe 'Visitor' do
   context 'logs in' do
     it 'user can fill out a form successfully' do
 
-      user = create(:user)
-
       visit '/'
 
       click_on 'Sign Up to Be a User'
 
       expect(current_path).to eq(new_user_path)
 
-      fill_in 'user[username]', with: user.username
-      fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: user.password
+      fill_in 'user[username]', with: 'jelly bean'
+      fill_in 'user[email]', with: 'something@gmail.com'
+      fill_in 'user[password]', with: 'password'
 
       click_on 'Create User'
 
       # save_and_open_page
-      # expect(page).to have_content("Welcome, #{user.username}!")
-      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content("Welcome, #{User.last.username}")
+      expect(current_path).to eq(user_path(User.last))
     end
   end
 end
@@ -28,6 +26,7 @@ end
 describe 'Visitor' do
   it 'user can sign out successfully' do
     user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit user_path(user)
 
