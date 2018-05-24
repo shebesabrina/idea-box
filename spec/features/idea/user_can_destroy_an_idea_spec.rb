890e5753ca68a05479fab32cab6506c85994d956
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-23 22:30:34 -0600

    Fixed destroy test with current user

--- spec/features/idea/user_can_destroy_an_idea_spec.rb
+++ spec/features/idea/user_can_destroy_an_idea_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe 'user destroy page' do
   it 'should allow user to delete an existing idea' do
     idea = create(:idea)
+    user = create(:user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit user_ideas_path(idea.user)
 
     click_on 'Delete'
 
-    expect(current_path).to eq(user_ideas_path(idea.user))
+    expect(current_path).to eq(user_ideas_path(user))
     expect(page).to_not have_content(idea)
   end
 end
