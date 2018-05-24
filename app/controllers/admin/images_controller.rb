0bfdb8ab877dcf94423a7519c6df923a4c1d1be1
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-23 23:45:20 -0600

    Adding image edit

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -10,6 +10,12 @@ class Admin::ImagesController < Admin::BaseController
     redirect_to images_path
   end
 
+  def destroy
+    image = Image.find(params[:id])
+    image.destroy
+    redirect_to admin_images_path
+  end
+
   private
 
   def image_params
